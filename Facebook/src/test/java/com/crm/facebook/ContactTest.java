c6d9555f6257664cff0990442e455100319c29b0
saash <saash@LAPTOP-6V2O8F5E>
2021-10-19 18:04:02 +0530

    first comit

---
+++ Facebook/src/test/java/com/crm/facebook/ContactTest.java
@@ -0,0 +1,11 @@
+package com.crm.facebook;
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void start()
+	{
+		System.out.println("hello this is first program");
+	}
+
+}
